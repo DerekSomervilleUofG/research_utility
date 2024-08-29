776ade143aef841e249c618e6645bc494a012d43
2213100S <2213100S@student.ac.uk>
2024-08-29 09:26:50 +0100

    Check if MS Teams config present

--- utility/Communication.py
+++ utility/Communication.py
@@ -5,10 +5,13 @@ class Communication():
     read_write_file = ReadWriteFile()
 
     def __init__(self) -> None:
-        key = self.read_write_file.get_file_as_string("ms_team.cfg", "resource/")
-        self.connector = pymsteams.connectorcard(key)
+        self.connector = None
+        if self.read_write_file.file_exists("ms_team.cfg", "resource/"):
+            key = self.read_write_file.get_file_as_string("ms_team.cfg", "resource/")
+            self.connector = pymsteams.connectorcard(key)
 
     def sent_ms_team_message(self, message):
-        self.connector.text(message)
-        self.connector.send()
+        if self.connector is not None:
+            self.connector.text(message)
+            self.connector.send()
 
