b48296a3e408e5303cac6b4f7c96bfaa80b54d47
2213100S <2213100S@student.ac.uk>
2024-08-29 14:00:21 +0100

    Email only if have config

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utility.Communication import Communication
 
 communication = Communication()
-communication.sent_ms_team_message("Del sent message")
+communication.send_ms_team_message("Del sent message")
+communication.send_email("derek.somerville@glasgow.ac.uk", "TEST", "Test message")
