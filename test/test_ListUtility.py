7a558a11e88929b5aac6abc4510e53f7fe89d34b
DerekSomervilleUofG <=>
2024-09-24 09:14:28 +0100

    Merge List test when same2

--- test/test_ListUtility.py
+++ test/test_ListUtility.py
@@ -8,3 +8,8 @@ class TestListUtility(TestCase):
         append_list = [3, 4, 5]
         self.assertEqual([1, 2, 3, 4, 5], ListUtility.merge_list(start_list, append_list))
     
+    
+    def test_merge_list_same(self):
+        start_list = [1, 2, 3]
+        append_list = [1, 2, 3]
+        self.assertEqual([1, 2, 3], ListUtility.merge_list(start_list, append_list))
