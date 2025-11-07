5457de6d63bc6c74bc4ae3a88e66a7a097e38a45
Derek Somerville <derek.somerville@glasgow.ac.uk>
2025-11-07 08:34:53 +0000

    Dictionary format

--- utility/DictUtility.py
+++ utility/DictUtility.py
@@ -2,3 +2,14 @@ class DictUtility:
 
     def dict_remove_items(original, duplicates):
         return {key: value for key, value in original.items() if key not in duplicates}
+    
+    def dict_format(data_dict):
+        content = str(data_dict)
+        replace_pairs = {
+            "{": "",
+            "}": "",
+            "[": "",
+            "]": "\n",
+            ": ": "\n",
+        }   
+        return content
